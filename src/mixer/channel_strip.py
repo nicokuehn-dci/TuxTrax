2efa13eda90db21566856838523d8b882d243640
nico.kuehn <nico.kuehn@dci-student.org>
2025-03-29 13:28:47 +0100

    Add volume and pan change signals to ChannelStrip class
    * Import `pyqtSignal` from `PyQt5.QtCore`
    * Add `volume_changed` and `pan_changed` signals to `ChannelStrip` class
    * Connect `volume_changed` signal to `volume_slider` value change
    * Connect `pan_changed` signal to `pan_slider` value change
    * Emit `volume_changed` and `pan_changed` signals with appropriate values

--- src/mixer/channel_strip.py
+++ src/mixer/channel_strip.py
@@ -1,7 +1,10 @@
 from PyQt5.QtWidgets import QWidget, QSlider, QLabel, QVBoxLayout
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, pyqtSignal
 
 class ChannelStrip(QWidget):
+    volume_changed = pyqtSignal(float)  # 0.0-1.0
+    pan_changed = pyqtSignal(float)     # -1.0 (left) to 1.0 (right)
+
     def __init__(self):
         super().__init__()
         
@@ -11,6 +14,7 @@ class ChannelStrip(QWidget):
         self.volume_slider = QSlider(Qt.Vertical)
         self.volume_slider.setRange(0, 100)
         self.volume_slider.setValue(75)
+        self.volume_slider.valueChanged.connect(self._on_volume_changed)
         layout.addWidget(QLabel("Volume"))
         layout.addWidget(self.volume_slider)
         
@@ -18,7 +22,14 @@ class ChannelStrip(QWidget):
         self.pan_slider = QSlider(Qt.Vertical)
         self.pan_slider.setRange(-50, 50)
         self.pan_slider.setValue(0)
+        self.pan_slider.valueChanged.connect(self._on_pan_changed)
         layout.addWidget(QLabel("Pan"))
         layout.addWidget(self.pan_slider)
         
         self.setLayout(layout)
+
+    def _on_volume_changed(self, value):
+        self.volume_changed.emit(value / 100.0)
+
+    def _on_pan_changed(self, value):
+        self.pan_changed.emit(value / 50.0)
