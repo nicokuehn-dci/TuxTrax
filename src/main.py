13c407cc62d03ba7b45df1a086293d4cafeee358
nico.kuehn <nico.kuehn@dci-student.org>
2025-03-29 13:26:59 +0100

    Import `sounddevice` module and use it in `AudioEngine` class
    * Import `sounddevice` module at the beginning of the file
    * Replace `sd` with `sounddevice` in the `AudioEngine` class
    * Add `AudioEngine` class with methods for audio processing and streaming

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import sys
+import sounddevice as sounddevice
 from PyQt5.QtWidgets import QApplication, QMainWindow, QDockWidget, QWidget
 from gui.elektron_menu import ElektronMenu
 from gui.performance_grid import PerformanceGrid
@@ -41,6 +42,38 @@ class MainWindow(QMainWindow):
 
     def __del__(self):
         self.midi_mapper.stop_listening()
+
+class AudioEngine:
+    def __init__(self, sr=48000, buffer_size=512):
+        self.sr = sr
+        self.buffer_size = buffer_size
+        self.mix_buffer = np.zeros((buffer_size, 2), dtype=np.float32)
+        self.lock = Lock()
+        self.fx_rack = Pedalboard()
+        
+        self.stream = sounddevice.OutputStream(
+            samplerate=sr,
+            blocksize=buffer_size,
+            callback=self._callback,
+            dtype='float32'
+        )
+
+    def _callback(self, outdata, frames, time, status):
+        with self.lock:
+            processed = self.fx_rack.process(self.mix_buffer, self.sr)
+            outdata[:] = processed
+            self.mix_buffer.fill(0)
+
+    def add_audio(self, audio):
+        with self.lock:
+            end = min(len(audio), self.buffer_size)
+            self.mix_buffer[:end] += audio[:end]
+
+    def start(self):
+        self.stream.start()
+
+    def stop(self):
+        self.stream.stop()
 
 def main():
     app = QApplication(sys.argv)
